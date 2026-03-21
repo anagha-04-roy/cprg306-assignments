3a8f4b774acf346f9e3d136ce674158f510af072
AnagRoy <anagha.roy@edu.sait.ca>
2026-03-20 20:21:15 -0600

    Updated Firebase Auth

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -13,20 +13,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-// Import the functions you need from the SDKs you need
-//import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAzspyTfl610vwedt4AdChxyLyVr8c096w",
-//   authDomain: "cprg306assignment-week9-anagha.firebaseapp.com",
-//   projectId: "cprg306assignment-week9-anagha",
-//   storageBucket: "cprg306assignment-week9-anagha.firebasestorage.app",
-//   messagingSenderId: "435850657662",
-//   appId: "1:435850657662:web:b79a2d4df75c9978603d2f"
-// };
-
-// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
